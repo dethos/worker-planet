417fad1009e0547015fa865eb3f050449f337a5c
Gonçalo Valério <gon@ovalerio.net>
2023-12-27 18:27:35 +0000

    upgrade vulnerable dependencies and add support for recent node versions

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,17 @@ const {
 } = require('wranglerjs-compat-webpack-plugin')
 
 module.exports = {
+  mode: 'production',
   entry: './index.js',
   plugins: [new WranglerJsCompatWebpackPlugin()],
+  resolve: {
+    fallback: {
+      stream: require.resolve('stream-browserify'),
+      http: require.resolve('stream-http'),
+      url: require.resolve('url/'),
+      http: require.resolve('stream-http'),
+      https: require.resolve('https-browserify'),
+      timers: require.resolve('timers-browserify'),
+    },
+  },
 }
